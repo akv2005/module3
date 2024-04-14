bbab729f6960f5098d2583f555749ea8e64425cd
akv05 <akv0571@gmail.com>
2024-04-14 23:02:55 +0900

    Домашняя работа по уроку "Пространство имен и способы вызова функции"
    Самостоятельная работа по уроку "Распаковка параметров и параметры функции"

---
+++ main3.py
@@ -0,0 +1,17 @@
+print('1---------------------')
+def test_1(nn=1, *types, names_="Имя, ", **dict):
+    print("Type:", type(dict))
+    print(nn,names_, "Фамилия:", *dict.values())
+    print(types)
+
+test_1(2, 3, 4, names='Иван', fam = 'Сидоров')
+
+print('2---------------------')
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+print('Факториалэл', factorial(6))
